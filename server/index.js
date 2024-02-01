3043b2c86de1c9b029e4c36ed344380c91f18efe
Monkey-D-MC1 <mahfuzc542@gmail.com>
2024-02-01 21:07:06 +0000

    reformat

--- server/index.js
+++ server/index.js
@@ -16,32 +16,46 @@ import { register } from "./controllers/auth.js";
 import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
 
+/* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.contentSecurityPolicy({
-    useDefaults: true,
-    directives: {
-      "script-src": ["'self'", "'unsafe-inline'"],
-    },
-  })
-);
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-
-const corsOptions = {
-  origin: ["https://retro-chirp.vercel.app", "https://retrochirp-api.onrender.com", "http://localhost:3000"]
-};
-
-app.use(cors(corsOptions));
-
+app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+app.get("/", (req, res) => {
+  res.send("Welcome to your Express API!");
+});
+/* FILE STORAGE */
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/assets");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage });
 
+/* ROUTES WITH FILES */
+app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
+
+/* ROUTES */
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
+app.use("/conversations", conversationRoutes);
+app.use("/messages", messageRoutes);
+
+
+/* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
 mongoose
   .connect(process.env.MONGO_URL, {
@@ -50,21 +64,9 @@ mongoose
   })
   .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+    /* ADD DATA ONE TIME */
+    // User.insertMany(users);
+    // Post.insertMany(posts);
   })
   .catch((error) => console.log(`${error} did not connect`));
-
-// Allow preflight requests
-app.options('*', cors(corsOptions));
-
-// Handle routes
-app.post("/auth/register", register);
-app.post("/posts", verifyToken, createPost);
-
-app.get("/", (req, res) => {
-  res.send("Welcome to your Express API!");
-});
-app.use("/auth", authRoutes);
-app.use("/users", userRoutes);
-app.use("/posts", postRoutes);
-app.use("/conversations", conversationRoutes);
-app.use("/messages", messageRoutes);

8f1dd871d994bd0e9fce65abab21d0d1a8931f0e
Monkey-D-MC1 <mahfuzc542@gmail.com>
2024-02-01 21:11:06 +0000

    fix

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
+import multer from "multer";
 import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
@@ -11,10 +12,15 @@ import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
 import conversationRoutes from "./routes/conversations.js";
-import messageRoutes from "./routes/messages.js";
+import messageRoutes from "./routes/messages.js"
 import { register } from "./controllers/auth.js";
 import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
+
+
+// import User from "./models/User.js";
+// import Post from "./models/Post.js";
+// import { users, posts } from "./data/index.js";
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -29,9 +35,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
-app.get("/", (req, res) => {
-  res.send("Welcome to your Express API!");
-});
+
 /* FILE STORAGE */
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -53,7 +57,6 @@ app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
 app.use("/conversations", conversationRoutes);
 app.use("/messages", messageRoutes);
-
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
